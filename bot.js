723059c7cea5f1f1e6bb482f96866b773606d800
NyrServer <47066160+NyrServer@users.noreply.github.com>
2019-02-14 15:38:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,14 +28,28 @@ client.on('message', function(message) {
 });
 
 
-	client.on('guildMemberAdd', member => {
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const logChannel = member.guild.channels.find(channel => channel.name === "invite-checker");
-    logChannel.send(`${member.user.tag} دخل باستخدام الانفايت ${invite.code} من قبل ${inviter.tag}. تم استخدام الانفايت ${invite.uses} مرة من حين انشائها.`);
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
   });
 });
 
